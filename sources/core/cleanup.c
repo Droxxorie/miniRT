19d0a8a627bbd9fe64c24e6abf2ae1b09f9beb23
Eloi <eloiraad@gmail.com>
2025-12-21 19:00:34 +0100

    added controls message

--- sources/core/cleanup.c
+++ sources/core/cleanup.c
@@ -6,7 +6,7 @@
 /*   By: eraad <eraad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/11 11:55:04 by eraad             #+#    #+#             */
-/*   Updated: 2025/12/21 17:25:59 by eraad            ###   ########.fr       */
+/*   Updated: 2025/12/21 18:58:30 by eraad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,8 @@ void	destroy_scene(t_scene *scene)
 {
 	if (!scene)
 		return ;
-	log_info("\nCleaning up scene resources...");
+	ft_putstr_fd("\n", 1);
+	log_info("Cleaning up scene resources...");
 	if (scene->mlx_window.mlx_ptr)
 	{
 		if (scene->frame_buffer.ptr)
--- sources/core/main.c
+++ sources/core/main.c
@@ -6,11 +6,38 @@
 /*   By: eraad <eraad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/07 14:07:55 by eraad             #+#    #+#             */
-/*   Updated: 2025/12/18 16:36:37 by eraad            ###   ########.fr       */
+/*   Updated: 2025/12/21 18:59:43 by eraad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minirt.h>
+
+static void	print_controls(void)
+{
+	ft_putstr_fd(B_YELLOW"", 1);
+	ft_putstr_fd("\n╔══════════════════════════════════════════════════╗\n", 1);
+	ft_putstr_fd("║                 miniRT CONTROLS                  ║\n", 1);
+	ft_putstr_fd("╚══════════════════════════════════════════════════╝\n", 1);
+	ft_putstr_fd("╔══════════════════════════════════════════════════╗\n", 1);
+	ft_putstr_fd("║                                     - GENERAL -  ║\n", 1);
+	ft_putstr_fd("║  [ESC]   Exit the program                        ║\n", 1);
+	ft_putstr_fd("║  [TAB]   Switch between ROT & TRANS mode         ║\n", 1);
+	ft_putstr_fd("║                                                  ║\n", 1);
+	ft_putstr_fd("║                                   - SELECTION -  ║\n", 1);
+	ft_putstr_fd("║  [L / R Click]   Select Object / Camera          ║\n", 1);
+	ft_putstr_fd("║  [L]             Select light source             ║\n", 1);
+	ft_putstr_fd("║                                                  ║\n", 1);
+	ft_putstr_fd("║                                    - MOVEMENT -  ║\n", 1);
+	ft_putstr_fd("║  [W / S]   Move Forward / Backward               ║\n", 1);
+	ft_putstr_fd("║  [A / D]   Move Left / Right                     ║\n", 1);
+	ft_putstr_fd("║  [Q / E]   Move Down / Up                        ║\n", 1);
+	ft_putstr_fd("║                                                  ║\n", 1);
+	ft_putstr_fd("║                                - MODIFICATION -  ║\n", 1);
+	ft_putstr_fd("║  [+ / -]             Resize Diameter             ║\n", 1);
+	ft_putstr_fd("║  [SHIFT] + [+ / -]   Resize Height (Cylinder)    ║\n", 1);
+	ft_putstr_fd("╚══════════════════════════════════════════════════╝\n", 1);
+	ft_putstr_fd(RESET "\n", 1);
+}
 
 int	main(int argc, char **argv)
 {
@@ -29,7 +56,8 @@ int	main(int argc, char **argv)
 	log_info("Initializing graphics...");
 	if (init_graphics(scene) == EXIT_FAILURE)
 		print_error_free_exit(scene, ERR_GRAPHICS);
-	log_info("Starting render loop...\n");
+	log_info("Starting render loop...");
+	print_controls();
 	if (render_frame(scene) == EXIT_FAILURE)
 		print_error_free_exit(scene, ERR_RENDER);
 	input_manager(scene);
